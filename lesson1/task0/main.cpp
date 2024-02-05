b1888f08be291453f2624402ff2bca88a21368ba
Born Villain <bornvillian@list.ru>
2024-02-05 23:17:05 +0300

    add lesson1

---
+++ lesson1/task0/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+class Calculator {
+public:
+
+	Calculator(double num1, double num2) {
+		this->num1 = num1;
+		this->num2 = num2;
+	}
+
+	double add() {
+		return num1 + num2;
+	}
+	double multiply() {
+		return num1 * num2;
+	}
+	double subtract_1_2() {
+		return num2 - num1;
+	}
+	double subtract_2_1() {
+		return num1 - num2;
+	}
+	double divide_1_2() {
+		return num1 / num2;
+	}
+	double divide_2_1() {
+		return num2 / num1;
+	}
+	bool set_num1(double num1) {
+		if (num1 != 0) {
+			this->num1 = num1;
+			return true;
+		}
+		return false;
+	}
+	bool set_num2(double num2) {
+		if (num2 != 0) {
+			this->num2 = num2;
+			return true;
+		}
+		return false;
+	}
+
+private:
+	double num1, num2;
+};
+
+int main() {
+	double num1, num2;
+	while (true) {
+		std::cout << "Введите num1: ";
+		std::cin >> num1;
+
+		do {
+			std::cout << "Введите num2: ";
+			std::cin >> num2;
+
+			if (num2 == 0) {
+				std::cout << "Неверный ввод!" << std::endl;
+			}
+		} while (num2 == 0);
+
+		Calculator calc(num1, num2);
+		std::cout << "num1 + num2 = " << calc.add() << std::endl;
+		std::cout << "num1 - num2 = " << calc.subtract_2_1() << std::endl;
+		std::cout << "num2 - num1 = " << calc.subtract_1_2() << std::endl;
+		std::cout << "num1 * num2 = " << calc.multiply() << std::endl;
+		std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
+		std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
+
+
+	}
+}
---
+++ lesson1/task1/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+class Counter {
+public:
+	Counter() {
+		value_ = 1;
+	}
+
+	Counter(int value) {
+		value_ = value;
+	}
+
+	void Increment() {
+		++value_;
+	}
+
+	void Decrement() {
+		--value_;
+	}
+
+	int GetValue() {
+		return value_;
+	}
+
+private:
+	int value_;
+
+};
+
+
+void InteractWithUser(Counter& c) {
+
+	do {
+		std::cout << "Введите команду: ";
+		char op;
+		std::cin >> op;
+
+		if (op == '+') {
+			c.Increment();
+		}
+		else if (op == '-') {
+			c.Decrement();
+		}
+		else if (op == '=') {
+			std::cout << c.GetValue() << std::endl;
+		}
+		else if (op == 'x') {
+			break;
+		}
+	} while (true);
+	std::cout << "До свидания!";
+}
+
+
+int main() {
+
+	std::cout << "Вы хотите указать начальное значение счётчика ? Введите да или нет: ";
+	std::string answer;
+	std::cin >> answer;
+	bool default_value;
+
+	if (answer == "да") {
+		std::cout << "Введите начальное значение счётчика: ";
+		int initial_value;
+		std::cin >> initial_value;
+		Counter c(initial_value);
+		InteractWithUser(c);
+	}
+	else if (answer == "нет") {
+		Counter c;
+		InteractWithUser(c);
+	}
+}
