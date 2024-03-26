850307b905e33d7f5e57a57f28a03b5c09d13ac2
Born Villain <bornvillian@list.ru>
2024-03-26 16:27:54 +0300

    add lesson1/task1/CMakeLists.txt

--- lesson1/task1/main.cpp
+++ lesson1/task1/main.cpp
@@ -2,72 +2,68 @@
 
 class Counter {
 public:
-	Counter() {
-		value_ = 1;
-	}
+    Counter() {
+        value_ = 1;
+    }
 
-	Counter(int value) {
-		value_ = value;
-	}
+    Counter(int value) {
+        value_ = value;
+    }
 
-	void Increment() {
-		++value_;
-	}
+    void Increment() {
+        ++value_;
+    }
 
-	void Decrement() {
-		--value_;
-	}
+    void Decrement() {
+        --value_;
+    }
 
-	int GetValue() {
-		return value_;
-	}
+    int GetValue() {
+        return value_;
+    }
 
 private:
-	int value_;
+    int value_;
 
 };
 
 
-void InteractWithUser(Counter& c) {
+void InteractWithUser(Counter &c) {
 
-	do {
-		std::cout << "Введите команду: ";
-		char op;
-		std::cin >> op;
+    do {
+        std::cout << "Введите команду: ";
+        char op;
+        std::cin >> op;
 
-		if (op == '+') {
-			c.Increment();
-		}
-		else if (op == '-') {
-			c.Decrement();
-		}
-		else if (op == '=') {
-			std::cout << c.GetValue() << std::endl;
-		}
-		else if (op == 'x') {
-			break;
-		}
-	} while (true);
-	std::cout << "До свидания!";
+        if (op == '+') {
+            c.Increment();
+        } else if (op == '-') {
+            c.Decrement();
+        } else if (op == '=') {
+            std::cout << c.GetValue() << std::endl;
+        } else if (op == 'x') {
+            break;
+        }
+    } while (true);
+    std::cout << "До свидания!";
 }
 
 
 int main() {
 
-	std::cout << "Вы хотите указать начальное значение счётчика ? Введите да или нет: ";
-	std::string answer;
-	std::cin >> answer;
-	bool default_value;
+    std::cout << "Вы хотите указать начальное значение счётчика ? Введите да или нет: ";
+    std::string answer;
+    std::cin >> answer;
+    bool default_value;
 
-	if (answer == "да") {
-		std::cout << "Введите начальное значение счётчика: ";
-		int initial_value;
-		std::cin >> initial_value;
-		Counter c(initial_value);
-		InteractWithUser(c);
-	}
-	else if (answer == "нет") {
-		Counter c;
-		InteractWithUser(c);
-	}
+    if (answer == "да") {
+        std::cout << "Введите начальное значение счётчика: ";
+        int initial_value;
+        std::cin >> initial_value;
+        Counter c(initial_value);
+        InteractWithUser(c);
+    } else if (answer == "нет") {
+        Counter c;
+        InteractWithUser(c);
+    }
 }
