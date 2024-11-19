7a06d39775b2c6941dc94798b871a4b1733f4f17
Born Villain <bornvillain@list.ru>
2024-11-19 17:16:21 +0300

    add simple conslole qt application

---
+++ qt/0/main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include <QSqlDatabase>
+#include <QHostInfo>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    QHostInfo info;
+    QSqlDatabase db;
+    return 0;
+}
